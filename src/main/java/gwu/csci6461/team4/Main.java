19eab06710e227d0754e75171026f280d501eb5d
Ruiyang Chen <im.ruiyangchen@gmail.com>
2024-02-01 20:46:09 -0500

    Create basic file structure

---
+++ src/main/java/gwu/csci6461/team4/Main.java
@@ -0,0 +1,13 @@
+package gwu.csci6461.team4;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // This is the main program of the project
+        System.out.println("Hello and welcome to the project of team4!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
