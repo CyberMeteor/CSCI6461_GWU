e97ebf97a6d2a456459116dfba89311a0db42863
bhavanathakare09 <157442929+bhavanathakare09@users.noreply.github.com>
2024-02-06 12:25:22 -0500

    Update Main.java

--- src/main/java/gwu/csci6461/team4/Main.java
+++ src/main/java/gwu/csci6461/team4/Main.java
@@ -6,7 +6,7 @@ import gwu.csci6461.team4.assembler.FileManager;
 public class Main {
     public static void main(String[] args) {
         FileManager fileManager = FileManager.getInstance();
-        fileManager.readLinesFromFile("input.txt");
+        fileManager.readLinesFromFile("input.txt"); //Reading input.txt file of instructions.
     }
 
 }
