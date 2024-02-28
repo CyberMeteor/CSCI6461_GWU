8695f7d9a91d17761b184787d391d6926eaa7cdf
Ruiyang Chen <im.ruiyangchen@gmail.com>
2024-02-27 22:01:35 -0500

    Upload the GUI of front panel

--- src/main/java/gwu/csci6461/team4/Main.java
+++ src/main/java/gwu/csci6461/team4/Main.java
@@ -2,11 +2,15 @@ package gwu.csci6461.team4;
 
 
 import gwu.csci6461.team4.assembler.FileManager;
+import gwu.csci6461.team4.simulator.FrontPanel;
+import gwu.csci6461.team4.simulator.SimulatorFrame;
 
 public class Main {
     public static void main(String[] args) {
-        FileManager fileManager = FileManager.getInstance();
-        fileManager.readLinesFromFile("input.txt"); //Reading input.txt file of instructions.
+//        FileManager fileManager = FileManager.getInstance();
+//        fileManager.readLinesFromFile("input.txt"); //Reading input.txt file of instructions.
+
+        SimulatorFrame simulatorFrame = new SimulatorFrame();
     }
 
 }
---
+++ src/main/java/gwu/csci6461/team4/simulator/SimulatorFrame.java
@@ -0,0 +1,20 @@
+package gwu.csci6461.team4.simulator;
+
+import javax.swing.*;
+
+public class SimulatorFrame extends JFrame {
+
+    public SimulatorFrame() {
+        super("Computer Architecture Simulator"); // Set the window title
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // completed FrontPanel code
+        FrontPanel frontPanel = new FrontPanel();
+        setContentPane(frontPanel);
+
+        pack();
+        setLocationRelativeTo(null);
+//        setSize(1100, 700); // Set initial width and height
+        setVisible(true);
+    }
+}
