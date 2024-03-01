df938a07ae6db985fadda2425ccd71989cd264e0
Ruiyang Chen <im.ruiyangchen@gmail.com>
2024-02-29 21:19:08 -0500

    Update the simulator controller

--- src/main/java/gwu/csci6461/team4/simulator/SimulatorPanelController.java
+++ src/main/java/gwu/csci6461/team4/simulator/SimulatorPanelController.java
@@ -5,15 +5,239 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class SimulatorPanelController {
-    @FXML
-    private Label GPR0Label;
 
-    @FXML
-    private TextField GPR0TextField;
-
+    // button onClick() function
     @FXML
     protected void GPR0LoadClick(){
         GPR0TextField.setText("000001");
     }
+
+    @FXML
+    protected void GPR1LoadClick(){
+        // TODO add handling code here:
+
+    }
+
+    @FXML
+    protected void GPR2LoadClick(){
+        // TODO add handling code here:
+
+    }
+    @FXML
+    protected void GPR3LoadClick(){
+        // TODO add handling code here:
+
+    }
+
+    @FXML
+    protected void IXR1LoadClick(){
+        // TODO add handling code here:
+
+    }
+
+    @FXML
+    protected void IXR2LoadClick(){
+        // TODO add handling code here:
+
+    }
+
+    @FXML
+    protected void IXR3LoadClick(){
+        // TODO add handling code here:
+
+    }
+
+    @FXML
+    protected void PCLoadClick(){
+        // TODO add handling code here:
+
+    }
+
+    @FXML
+    protected void MARLoadClick(){
+        // TODO add handling code here:
+
+    }
+
+    @FXML
+    protected void MBRLoadClick(){
+        // TODO add handling code here:
+
+    }
+
+    @FXML
+    protected void LoadClick(){
+        // TODO add handling code here:
+
+    }
+
+    @FXML
+    protected void LoadPlusClick(){
+        // TODO add handling code here:
+
+    }
+
+    @FXML
+    protected void StoreClick(){
+        // TODO add handling code here:
+
+    }
+
+    @FXML
+    protected void StorePlusClick(){
+        // TODO add handling code here:
+
+    }
+
+    @FXML
+    protected void RunClick(){
+        // TODO add handling code here:
+
+    }
+
+    @FXML
+    protected void StepClick(){
+        // TODO add handling code here:
+
+    }
+
+    @FXML
+    protected void HaltClick(){
+        // TODO add handling code here:
+
+    }
+
+    @FXML
+    protected void IPLClick(){
+        // TODO add handling code here:
+
+    }
+
+
+    // Labels
+    @FXML
+    private Label GPR0Label;
+
+    @FXML
+    private Label GPR1Label;
+
+    @FXML
+    private Label GPR2Label;
+
+    @FXML
+    private Label GPR3Label;
+
+    @FXML
+    private Label IXR1Label;
+
+    @FXML
+    private Label IXR2Label;
+
+    @FXML
+    private Label IXR3Label;
+
+    @FXML
+    private Label PCLabel;
+
+    @FXML
+    private Label MARLabel;
+
+    @FXML
+    private Label MBRLabel;
+
+    @FXML
+    private Label IRLabel;
+
+    @FXML
+    private Label CCLabel;
+
+    @FXML
+    private Label MFRLabel;
+
+    @FXML
+    private Label LoadLabel;
+
+    @FXML
+    private Label LoadPlusLabel;
+
+    @FXML
+    private Label StoreLabel;
+
+    @FXML
+    private Label StorePlusLabel;
+
+    @FXML
+    private Label RunLabel;
+
+    @FXML
+    private Label StepLabel;
+
+    @FXML
+    private Label HaltLabel;
+
+    @FXML
+    private Label OctalInputLabel;
+
+    @FXML
+    private Label BinaryLabel;
+
+    @FXML
+    private Label IPLLabel;
+
+    @FXML
+    private Label ProgramFileLabel;
+
+    @FXML
+    private Label SimulatorTitleLabel;
+
+
+    //TextFields
+    @FXML
+    private TextField GPR0TextField;
+
+    @FXML
+    private TextField GPR1TextField;
+
+    @FXML
+    private TextField GPR2TextField;
+
+    @FXML
+    private TextField GPR3TextField;
+
+    @FXML
+    private TextField IXR1TextField;
+
+    @FXML
+    private TextField IXR2TextField;
+
+    @FXML
+    private TextField IXR3TextField;
+
+    @FXML
+    private TextField PCTextField;
+
+    @FXML
+    private TextField MARTextField;
+
+    @FXML
+    private TextField MBRTextField;
+
+    @FXML
+    private TextField IRTextField;
+
+    @FXML
+    private TextField CCTextField;
+
+    @FXML
+    private TextField MFRTextField;
+
+    @FXML
+    private TextField OctalInputTextField;
+
+    @FXML
+    private TextField BinaryTextField;
+
+    @FXML
+    private TextField ProgramFileTextField;
 
 }
